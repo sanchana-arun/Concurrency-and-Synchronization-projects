51f0343bdc98b67789e08d9275a9123d25aed2d8
Sanchana Arun <130022933+sanchana-arun@users.noreply.github.com>
2024-07-15 01:15:07 +0530

    Create rwlock.h

---
+++ rwlock.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+
+struct read_write_lock
+{
+    pthread_mutex_t lock;
+    pthread_cond_t readers;
+    pthread_cond_t writers;
+    int active_readers;
+    int waiting_writers;
+    int active_writers;
+};
+
+void InitalizeReadWriteLock(struct read_write_lock * rw);
+void ReaderLock(struct read_write_lock * rw);
+void ReaderUnlock(struct read_write_lock * rw);
+void WriterLock(struct read_write_lock * rw);
+void WriterUnlock(struct read_write_lock * rw);
